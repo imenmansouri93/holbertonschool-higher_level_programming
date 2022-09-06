8581239854a737cc5ea80254e5b24447612439c3
imenmansouri93 <imennmansourii@gmail.com>
2022-09-06 14:06:59 +0100

    printstring

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,4 +3,4 @@ word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"First 3 letters: {word[0:3]}")
 print(f"Last 2 letters: {word[7:]}")
-print(f"Middle word: {word[1:8]}")
+print(f"Middle word: {word[1:-1]}")
