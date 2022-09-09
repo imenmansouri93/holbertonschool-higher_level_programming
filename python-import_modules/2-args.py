17102add64556d27c3482d8abf734c8c75027efc
imenmansouri93 <imennmansourii@gmail.com>
2022-09-09 23:18:59 +0100

    prints the number of and the list of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 import sys
 if __name__ == "__main__":
     av = len(sys.argv)
-for i in range(1, av):
-    print("{} {}".format(i, sys.argv[i]))
 if av == 1:
     print("{} arguments.".format(av -  1))
 elif av == 2:
     print("{} arguments.".format(av -  1))
 else:
     print("{} arguments.".format(av -  1))
+for i in range(1, av):
+    print("{}: {}".format(i, sys.argv[i]))
