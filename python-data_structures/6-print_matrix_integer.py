2f716e002c50795f1f66ea7bd68b8b7b848230fc
imenmansouri93 <imennmansourii@gmail.com>
2022-09-13 23:09:25 +0100

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        i = 0
+   for row in matrix:
+    i = 0
     for j in row:
         i += 1
         print("{:d}".format(j), end="")
