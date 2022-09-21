30dc48ce4416665cc735b93a9ea70845018db03d
imenmansouri93 <imennmansourii@gmail.com>
2022-09-21 23:14:43 +0100

    Real definition of a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Real definition of a rectangle"""
-    
+
+
 class Rectangle:
     """Represent a Rectangle"""
-    def __init__(self,width=0, height=0):
+    def __init__(self, width=0, height=0):
         """
         Create a new rectangle of width  and height.
         Args:
@@ -12,10 +13,12 @@ class Rectangle:
         """
         self.__width = width
         self.__height = height
+
     @property
     def width(self):
         """get the width of the rectangle"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """set the width of the rectangle"""
@@ -24,10 +27,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         """get the height of the rectangle"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """set the height of the rectangle"""
