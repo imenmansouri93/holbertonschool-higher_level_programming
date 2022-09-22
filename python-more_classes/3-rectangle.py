59833f2aa175ce9f34b3084e954946322664443d
imenmansouri93 <imennmansourii@gmail.com>
2022-09-22 11:22:19 +0100

    first commit

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle():
         if self.__height == 0 or self.__width == 0:
             return 0
         return ((self.__height + self.__width) * 2)
-    
+
     def __str__(self):
         """print rectangle"""
         string = ""
