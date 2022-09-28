1e92cc4f7c0d81b754f84eaab42f3c805b249a71
imenmansouri93 <imennmansourii@gmail.com>
2022-09-28 16:09:50 +0100

    first commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define a base geometry class"""
+
+class BaseGeometry:
+    """Represent BaseGeometry
+    """
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
