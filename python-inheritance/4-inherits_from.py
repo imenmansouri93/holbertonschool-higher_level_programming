8266984a3377d942dceb418898b0bb244cdd34a9
imenmansouri93 <imennmansourii@gmail.com>
2022-09-29 14:58:53 +0100

    first commit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,7 @@ def inherits_from(obj, a_class):
     """
     aaaaaa
     """
-    return isinstance(type(obj), a_class) and obj != a_class
+    if type(obj) == a_class:
+     return (False)
+    else:
+        return isinstance(type(obj), a_class)
