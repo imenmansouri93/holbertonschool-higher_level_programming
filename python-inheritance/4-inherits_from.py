d6d7a89d86c2544b23a574397e45c26a3199c37b
imenmansouri93 <imennmansourii@gmail.com>
2022-09-29 15:01:39 +0100

    first commit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""aaaa"""
+"""Only sub class"""
 
 def inherits_from(obj, a_class):
-    """
-    aaaaaa
+    """returns True if the object is an instance of
+    a class that inherited (directly or indirectly)
+    from the specified class 
     """
     if type(obj) == a_class:
      return (False)
