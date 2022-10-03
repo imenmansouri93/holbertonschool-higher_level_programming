50865f7e1f899253f5f4e04c5f67670ce67955c4
imenmansouri93 <imennmansourii@gmail.com>
2022-10-03 19:54:40 +0100

    first commit

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """JSON string"""
 import json
+
+
 def to_json_string(my_obj):
     """ function that returns the JSON representation of an object """
     return json.dumps(my_obj)
