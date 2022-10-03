2ac7774f8f241c2229ec843e7cdf935d17589b6c
imenmansouri93 <imennmansourii@gmail.com>
2022-10-03 17:36:00 +0100

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""Read file"""
+
+
 def read_file(filename=""):
+    """function that reads a text file """
     with open("my_file_0.txt", mode="r", encoding="utf-8") as myFile:
         print(myFile.read())
