4ff6630fa8292972bc973cc388b18d25cfa87268
imenmansouri93 <imennmansourii@gmail.com>
2022-10-03 18:52:46 +0100

    first commit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,6 +2,6 @@
 """Write to a file"""
 def write_file(filename="", text=""):
     """ function that writes a string to a text file"""
-    with open("filename", mode="w", encoding="utf-8") as myFile:
+    with open(filename, mode="w", encoding="utf-8") as myFile:
         return(myFile.write(text))
         
