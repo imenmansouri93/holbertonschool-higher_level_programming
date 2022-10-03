27e5d94b11cf55041297dec231bed829c40d6821
imenmansouri93 <imennmansourii@gmail.com>
2022-10-03 19:35:32 +0100

    first commit

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""Append to a file"""
 
 
 def append_write(filename="", text=""):
-    """"""
+    """function that appends a string at the end of a text file """
     with open(filename, mode="a", encoding="utf-8") as myFile:
         return(myFile.write(text))
