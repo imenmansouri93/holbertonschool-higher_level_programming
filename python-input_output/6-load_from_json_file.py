7f92c3c8d0e9a70d30c6e37dcc9f7eb6a1008156
imenmansouri93 <imennmansourii@gmail.com>
2022-10-04 17:08:42 +0100

    first commit

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,6 +2,7 @@
 """Create object from a JSON file"""
 import json
 
+
 def load_from_json_file(filename):
     """function that creates an Object from a “JSON file"""
     with open(filename, encoding='utf-8') as myFile:
