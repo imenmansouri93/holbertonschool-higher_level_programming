0a796948d644e13332d31436308ed3e72e03f4ff
imenmansouri93 <imennmansourii@gmail.com>
2022-10-06 10:15:33 +0100

    new

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,16 +6,16 @@ class Rectangle(Base):
     """class Rectangle that inherits from Base
     """
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super().__init__(id)
 
 @property
 def width(self):
     """set/get the width of the  reectangle"""
-    return (self.__width)
+    return (self.width)
 
 @width.setter
 def width(self,value):
@@ -23,7 +23,7 @@ def width(self,value):
         raise TypeError("width must be an integer")
     if value < 0:
         raise  ValueError("width must be >= 0")
-    self.__width = value
+    self.width = value
 
 @property
 def height(self):
