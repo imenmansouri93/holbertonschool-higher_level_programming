7c10045a006f5c6247d14b152469c1d5d8a99082
imenmansouri93 <imennmansourii@gmail.com>
2022-10-06 14:20:18 +0100

    new

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,5 +74,6 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        """method so that it returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return ("[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id, self.x,  self.y,self.width,self.height ))
+        """method so that it returns [Rectangle]
+        (<id>) <x>/<y> - <width>/<height>"""
+        return ("[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id, self.x,  self.y,self.width,self.height))
