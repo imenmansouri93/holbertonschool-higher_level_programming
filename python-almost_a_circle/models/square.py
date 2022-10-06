7669ec3f89bdcc5409f1e5ed02f9a121da19edc1
imenmansouri93 <imennmansourii@gmail.com>
2022-10-06 19:32:09 +0100

    new

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,3 +12,14 @@ class Square(Rectangle):
         """method should return [Square] (<id>) <x>/<y> - <size>"""
         return ("[Square] ({:d}) {:d}/{:d} - {:d}".format(
             self.id, self.x, self.y, self.width))
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.width = value
