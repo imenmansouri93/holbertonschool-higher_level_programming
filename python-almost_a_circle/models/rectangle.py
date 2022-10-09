6371664aa616466d822d1c0451bdcee067454967
imenmansouri93 <imennmansourii@gmail.com>
2022-10-09 18:51:21 +0100

    first commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -70,11 +70,12 @@ class Rectangle(Base):
     def display(self):
         """that prints in stdout the Rectangle instance with the character #"""
         if self.__y > 0:
-            print('\n' * self.__y, end= '')
+            print('\n' * self.__y,end='')
         for i in range(self.__height):
             if self.__x > 0:
-                print(" " * self.__x, end = '')
-            print ("#" *  self.__width)
+                print(" " * self.__x,end='')
+            print ("#" * self.__width)
+
     def __str__(self):
         """method so that it returns [Rectangle]
         (<id>) <x>/<y> - <width>/<height>"""
