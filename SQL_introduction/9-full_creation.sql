1f6b274705a25ac999fb2f8bf16b1bf32d97cbfb
imenmansouri93 <imennmansourii@gmail.com>
2022-11-03 23:59:30 +0100

    new

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+--
+CREATE TABLE second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table`(`id`, `name`,`score`)
+VALUES
+
+    (1, "john", 10),
+    (2, "ALEX", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
