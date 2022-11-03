2d9fcf81916b48cba998a7f9e6b950545b2e7e23
imenmansouri93 <imennmansourii@gmail.com>
2022-11-04 00:02:20 +0100

    new

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
 --
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO `second_table`(`id`, `name`,`score`)
+CREATE TABLE IF NOT EXISTS second_table
+(id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table`(`id`, `name`, `score`)
 VALUES
 
     (1, "john", 10),
