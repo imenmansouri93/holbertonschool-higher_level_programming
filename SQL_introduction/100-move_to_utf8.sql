dc56440cb7a83d66d8ce92f9d35255121fd5ab4e
imenmansouri93 <imennmansourii@gmail.com>
2022-11-04 16:09:37 +0100

    new

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- converts hbtn_0c_0 database to UTF8 
 
-ALTER TABLE first_table CHANGE name VARCHAR(256) CHARACTER SET utf8mb4;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
 ALTER TABLE first_table CONVERT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
