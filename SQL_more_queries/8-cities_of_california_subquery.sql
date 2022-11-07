3dd6fbd04ce473261be5cf3710c8d5de82e352d7
imenmansouri93 <imennmansourii@gmail.com>
2022-11-07 21:06:15 +0100

    new

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,6 @@
 -- lists all the cities of California that can be found in the database
-SELECT * FROM cities
+SELECT 
+    id , name
+FROM cities
 WHERE state_id = (SELECT id FROM states WHERE  name = 'Californi')
 ORDER BY id  ASC;
