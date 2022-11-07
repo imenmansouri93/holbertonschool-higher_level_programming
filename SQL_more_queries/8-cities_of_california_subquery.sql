2eade45321b41d338a1829921a3e254ece816057
imenmansouri93 <imennmansourii@gmail.com>
2022-11-07 21:18:22 +0100

    new

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 SELECT 
     `id` , `name`
 FROM cities
-WHERE `state_id` = (SELECT `id` FROM `states` WHERE  `name` = 'Californi')
+WHERE `state_id` IN (SELECT `id` FROM `states` WHERE  `name` = 'Californi')
 ORDER BY `id`  ASC;
