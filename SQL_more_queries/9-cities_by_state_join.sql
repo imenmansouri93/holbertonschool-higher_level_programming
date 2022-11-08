0b184278c55da7fb7e4c6070589407ec7163e8f9
imenmansouri93 <imennmansourii@gmail.com>
2022-11-08 19:06:15 +0100

    new

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
 FROM cities
-WHERE join states ON cities.state_id = states.id
+WHERE JOIN states ON cities.state_id = states.id
