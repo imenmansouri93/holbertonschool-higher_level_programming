d34bd65af9f1d8086a922e28398cb78603ed3165
imenmansouri93 <imennmansourii@gmail.com>
2023-04-21 16:51:31 +0100

    new

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function factoriel (a) {
+    if (a === 1) {
+      return a;
+    }
+    return a * factoriel(a - 1);
+  }
+  const process = require('process');
+  const args = process.argv;
+  if (isNaN(args[2])) {
+    console.log('1');
+  } else {
+    const fac = args[2];
+    console.log(factoriel(fac));
+  }
+  
