ed0e8228b8cf31b69c7ba826caef5e6537290e2a
imenmansouri93 <imennmansourii@gmail.com>
2023-04-24 21:56:46 +0100

    new

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const request = require('request')
+
+request(process.argv[2], function(err, response) {
+    console.log('code:', response.statusCode); //print response status code
+})
