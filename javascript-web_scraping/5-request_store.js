ab678c9be82b82d4153b556086ca55be7b56f9b9
imenmansouri93 <imennmansourii@gmail.com>
2023-04-27 16:07:57 +0100

    new

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require("request");
+const fs = require('fs');
+url = process.argv[2];
+const file = process.argv[3]
+
+request(url, function(err, response, body) {
+    if (err) {
+        console.log(err);
+    }
+    fs.writeFile(file, body, 'utf8', function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
